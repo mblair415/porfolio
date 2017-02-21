3a748a4d08b623edf04399269bd217ea33f956fe
mblair415 <mblair415@gmail.com>
2017-02-20 18:39:51 -0800

    minor css changes and a fix to click on a project image to hide the text

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
 
   // click on project image, show text for that image.  hide text for others.
   $('#project1').on('click', function() {
-    console.log('click project 1 image');
-    $('#project1Text').removeClass('hide').addClass('show');
-    $('#project2Text').removeClass('show').addClass('hide');
+    if ($('#project1Text').hasClass('hide')) {
+      $('#project1Text').removeClass('hide').addClass('show');
+      $('#project2Text').removeClass('show').addClass('hide');
+    } else {
+      $('#project1Text').removeClass('show').addClass('hide');
+    }
   })
 
   $('#project1MoreDetailsLink').on('click', function() {
